e15ccbb32e625f5749b9079f772d9d0fe91a6e6e
mqyang <3193372970@qq.com>
2019-06-04 17:27:11 +0800

    第一次提交

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import { Button } from 'antd-mobile';
+// import 'antd-mobile/dist/antd-mobile.css'; 
+import { Drawer, List, NavBar, Icon } from 'antd-mobile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+    state = {
+      open: true,
+    }
+    onOpenChange = (...args) => {
+      console.log(args);
+      this.setState({ open: !this.state.open });
+    }
+    render() {
+      // fix in codepen
+      const sidebar = (<List>
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((i, index) => {
+          if (index === 0) {
+            return (<List.Item key={index}
+              thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+              multipleLine
+            >Category</List.Item>);
+          }
+          return (<List.Item key={index}
+            thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+          >Category{index}</List.Item>);
+        })}
+      </List>);
+  
+      return (<div>
+        <NavBar icon={<Icon type="ellipsis" />} onLeftClick={this.onOpenChange}>Basic</NavBar>
+        <Drawer
+          className="my-drawer"
+          style={{ minHeight: document.documentElement.clientHeight }}
+          enableDragHandle
+          contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42 }}
+          sidebar={sidebar}
+          open={this.state.open}
+          onOpenChange={this.onOpenChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Click upper-left corner
+        </Drawer>
+      </div>);
+    }
 }
 
 export default App;
